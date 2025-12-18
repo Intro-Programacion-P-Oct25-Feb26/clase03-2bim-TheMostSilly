eed00bf693aa685f92a6388eb406278b79bc465f
TheMostSilly <geovannacastro058@gmail.com>
2025-12-18 10:55:13 -0500

    EJERCICIO

--- Ejemplosc3/src/paquete05/Ejercicio.java
+++ Ejemplosc3/src/paquete05/Ejercicio.java
@@ -18,31 +18,44 @@ public class Ejercicio {
         String[] nombres = new String[5];
         int[] produccion = new int[5];
         String reporte = "";
+        int mayor = produccion[0];
+        int mejor = 0;
 
-        for (int fila = 0; fila < estaciones.length; fila++) {
-            
+        for (int fila = 0; fila < estaciones[fila].length; fila++) {
+
             System.out.println("Ingrese el nombre de la estación");
             nombres[fila] = entrada.nextLine();
             int suma = 0;
             for (int col = 0; col < estaciones[fila].length; col++) {
-                System.out.println("Ingrese la producción de la estación");
+                System.out.printf("Ingrese la producción de la estación %s\n", col
+                        + 1);
                 int prod = entrada.nextInt();
                 entrada.nextLine();
                 suma = suma + prod;
                 estaciones[fila][col] = prod;
-               
 
             }
             produccion[fila] = suma;
-            reporte = String.format("%sNombre Estación %s - Total Producción:"
-                    + "$%s\n", reporte, nombres[fila], produccion[fila]);
-            
 
         }
+        for (int fila = 0; fila < estaciones.length; fila++) {
+            if (produccion[fila] > mayor) {
+                mayor = produccion[fila];
+                mejor = fila;
 
-        
+            }
+        }
+
+        for (int fila = 0; fila < estaciones.length; fila++) {
+            reporte = String.format("%s%sNombre Estación %s - Total Producción:"
+                    + "$%s\n", reporte, nombres[fila], fila + 1, produccion[fila]);
 
         }
+        reporte = String.format("%s\nEstación más productiva: Estación %s\nEncargado"
+                + "de la estación: %s\n"
+                + "Cantidad de la estación mas productiva: $%s", reporte,
+                mejor + 1, nombres[mejor], produccion[mejor]);
+        System.out.println(reporte);
+
     }
-
-
+}
